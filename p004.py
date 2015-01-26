0048ccce18ad72e7a13064c3fe1828d39bfbb0ce
Daniel Strong <dstrong@glyx.co.uk>
2015-01-26 20:28:45 +0000

    Fix p004

--- p004.py
+++ p004.py
@@ -25,8 +25,8 @@ def solve():
     '''
     Solve the problem.
     '''
-    return [i*j for i in range(100, 1000) for j in range(100, 1000) if
-            is_palindrome(i*j)]
+    return max([i*j for i in range(100, 1000) for j in range(100, 1000) if
+                is_palindrome(i*j)])
 
 if __name__ == '__main__':
     print(solve())
